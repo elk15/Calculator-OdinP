4b40c81511e23b65d0039d31935a8aacbd2ab313
elina <papadelina145@gmail.com>
2023-02-05 11:36:40 +0200

    Add operate function that calls basic math functions

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 // Basic Math Functions
 const add = (a, b) => {
     return a + b;
-}
+};
 
 const subtract = (a, b) => {
     return a - b;
-}
+};
 
 const multiply = (a, b) => {
     return a * b;
-}
+};
 
 const divide = (a, b) => {
     return a / b;
-}
+};
+
+//Operate function takes an operator and 2 numbers and calls one of the above functions
+
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    };
+};
