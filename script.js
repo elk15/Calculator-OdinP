2d383ece2bd8324302fe612193ef2681590d9fba
elina <papadelina145@gmail.com>
2023-02-05 20:16:41 +0200

    Implement equal button's functionality

--- script.js
+++ script.js
@@ -30,19 +30,40 @@ const operate = (operator, a, b) => {
     };
 };
 
+const calculate = (string) => {
+    let arr = string.split('');
+    let operator = '';
+    let first = '';
+    let value = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].match(/\+|-|x|\//)) {
+            operator = arr[i];
+            first = value;
+            value = '';
+        } else {
+            value += arr[i];
+        }
+    };
+    console.log(operator);
+    return operate(operator, parseInt(first), parseInt(value));
+};
+
 //Display functions
 const result_display = document.getElementById('result');
 const stored_display = document.getElementById('stored');
+let result = 0
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', event => {
-        if (button.classList.contains('num')) {
-            stored_display.innerText += button.innerText
-        };
-        if (button.classList.contains('op')) {
-            stored_display.innerText += button.innerText
-        };
+        if (stored_display.innerText.length < 10) {
+            if (button.classList.contains('num')) {
+                stored_display.innerText += button.innerText;
+            };
+            if (button.classList.contains('op')) {
+                stored_display.innerText += button.innerText;
+            };
+        }
         if (button.classList.contains('del')) {
             stored_display.innerText = stored_display.innerText.slice(0, -1);
         };
@@ -51,8 +72,16 @@ buttons.forEach(button => {
             stored_display.innerText = '';
         };
         if (button.classList.contains('equal')) {
-
+            if (stored_display.innerText.match(/^\d+(\+|-|x|\/)\d+$/)) {
+                result_display.innerText = '';
+                result = calculate(stored_display.innerText).toString();
+                if (result.length > 15) {
+                    result = result.slice(0, 15);
+                }
+                result_display.innerText += result;
+            }
         }
+
 
     });
 });
