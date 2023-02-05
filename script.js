b6764ead544a39c74e36ff3a228658eab716ad43
elina <papadelina145@gmail.com>
2023-02-05 13:41:36 +0200

    Implement functionality for buttons and pannel

--- script.js
+++ script.js
@@ -23,9 +23,39 @@ const operate = (operator, a, b) => {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
         case '/':
             return divide(a, b);
     };
 };
+
+//Display functions
+const active_display = document.getElementById('active');
+const stored_display = document.getElementById('stored');
+let stored_value = [];
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        if (button.classList.contains('op') || button.classList.contains('equal')) {
+            if (active_display.innerText != null) {
+                stored_value.push(parseInt(active_display.innerText));
+                stored_value.push(button.innerText);
+                stored_display.innerText = stored_value.join(' ');
+                active_display.innerText = ''
+            };
+        } else if (button.classList.contains('del')) {
+            active_display.innerText = active_display.innerText.slice(0, -1);
+        } else if (button.classList.contains('clear')) {
+            active_display.innerText = '';
+            stored_display.innerText = '';
+            stored_value = [];
+        } else {
+            active_display.innerText += button.innerText;
+
+        };
+        console.log(stored_value);
+
+    });
+});
